08b8aad5cfaf03415f40b1d87796e395e9c6cb79
rekomc <sajadmushtag@gmail.com>
2025-01-11 23:34:55 +0300

    Update verify.js

--- verify.js
+++ verify.js
@@ -1,5 +1,5 @@
 module.exports = {
     code: "discord.gg/wicks",
-    token: "MTMyNzY2MTIxNTg4MjkzNjM4Mg.GTLIB8._WMt4_XKE_K4rUbIiAaKbZ1LWtS2M0SaQ2IyaA",
+    token: "MTMyNzcyMTM4MDE3ODgyNTIxNw.GVx9MP.zB4dw9uQyWlizPhmjZBEhh1F_45gDatEJP_pDk",
     PORT:3000
 };
